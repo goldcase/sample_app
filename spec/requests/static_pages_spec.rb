4d994d47ca8f4bdcbf89124bc4ee5b3c319c816b
Johnny Chang <johnny@johnnychang.com>
2014-04-06 08:46:32 -0500

    finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+    it "should have the h1 'Home'" do
+    	visit '/static_pages/home'
+    	page.should have_selector('h1', :text => 'Home')
+    end
+	  it "should ahve hte right title" do
+	  	visit '/static_pages/home'
+	  	page.should have_selector('title', :text => " | Home")
+	  end
+  end
+  describe "Help page" do
+  	it "should ahve hte content 'Help'" do
+  		visit '/static_pages/help'
+  		page.should have_selector('h1', :text => 'Help')
+  	end
+	  it "should ahve hte right title" do
+	  	visit '/static_pages/help'
+	  	page.should have_selector('title', :text => " | Help")
+	  end
+  end
+  describe "About page" do
+  	it "should have the h1 'About Us'" do
+  		visit '/static_pages/about'
+  		page.should have_selector('h1', :text => "About Us")
+	end
+	  it "should ahve hte right title" do
+	  	visit '/static_pages/about'
+	  	page.should have_selector('title', :text => " | About Us")
+	end
+  end
+end
